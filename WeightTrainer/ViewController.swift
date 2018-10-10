a9f95db0110fee85f1ca4e244039606f2a2949fb
Matthew D Rainville <mrainville@eastbay.com>
2018-10-10 15:13:28 -0500

    Initial Commit

--- WeightTrainer/ViewController.swift
+++ WeightTrainer/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Programs: UIButton!
+    @IBOutlet weak var Exercises: UIButton!
+    @IBOutlet weak var About: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func navigateMenu(_ sender: UIButton) {
+        switch sender {
+        case Programs:
+            print("Programs")
+        case Exercises:
+            print("Exercises")
+        case About:
+            print("About")
+        default:
+            break
+        }
+    }
 }
 
